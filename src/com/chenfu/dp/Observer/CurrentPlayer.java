9503001d3129d9119ec6397c2a5c48a6de71ba83
chenfu2017 <211147056@qq.com>
2018-04-19 12:49:05 +0800

    Revert "add"
    This reverts commit e960b67

--- src/com/chenfu/dp/Observer/CurrentPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.chenfu.dp.Observer;
-
-public class CurrentPlayer extends Player {
-    public CurrentPlayer(String name) {
-        super(name);
-    }
-}
--- src/com/chenfu/dp/Observer/Teammate.java
+++
@@ -1,7 +0,0 @@
-package com.chenfu.dp.Observer;
-
-public class Teammate extends Player {
-    public Teammate(String name) {
-        super(name);
-    }
-}
