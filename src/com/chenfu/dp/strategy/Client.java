aadf8d435f333438e4aeb67a8486c0463bf49d90
chenfu <211147056@qq.com>
2018-05-23 07:54:55 +0800

    modify

--- src/com/chenfu/dp/strategy/Client.java
+++ src/com/chenfu/dp/strategy/Client.java
@@ -8,6 +8,7 @@ public class Client {
         int[] arr = {49,38,65,97,76,13,27,49,55,4};
         AbstractSort sort = new MergeSort();
         Context context = new Context(sort);
+
         System.out.println("排列前:"+Arrays.toString(arr));
         context.sort(arr);
         System.out.println("排列后:"+Arrays.toString(arr));
